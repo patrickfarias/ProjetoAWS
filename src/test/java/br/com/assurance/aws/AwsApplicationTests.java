60431b010c6da77c2fa8aabc07c55809a22ee753
patrickfarias <patrickfarias@icloud.com>
2026-06-25 16:17:40 -0300

    Add Projeto

---
+++ src/test/java/br/com/assurance/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.assurance.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
