372cd39f9446ea1ae154eefd1724e6d2f6c917d5
patrickfarias <patrickfarias@icloud.com>
2026-06-25 22:57:51 -0300

    Add Projeto

---
+++ src/main/java/br/com/assurance/aws/dynamo/config/DynamoConfig.java
@@ -0,0 +1,19 @@
+package br.com.assurance.aws.dynamo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoConfig {
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
